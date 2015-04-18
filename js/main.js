6af608224286f924f4353e2a78dc07ef6e867e8e
Conway Hyacienth <conway@zookeemedia.co.uk>
2015-04-18 01:47:34 +0100

    pretend data starting to take shape
    amount and date/time looped out of an object

--- js/main.js
+++ js/main.js
@@ -9,7 +9,12 @@ $(document).ready(function() {
 
 
 function ctrl($scope){
-  $scope.rows = [];
+  $scope.rows = {"1234":"17-04-2015:15.30pm", "745":"17-04-2015:16:47pm"};
+  $scope.ol = Object.keys($scope.rows).length;
+  //$scope.rows.push{amount:36,date:'17/04/2015'}; //{amount:52,date:'17/04/2015'},{amount:29,date:'17/04/2015'}
+
+  console.info($scope.rows);
+
   $scope.temp = false;
   
   $scope.addRow = function(){
@@ -45,10 +50,34 @@ function ctrl($scope){
   $scope.creditTemp = function(){
     if($scope.temp) $scope.rows.pop(); 
     else if($scope.creditAmount) $scope.temp = true;
-    
+  
+    a = +$scope.creditAmount;
+    rightnow = $scope.todaysDate();
+    //if($scope.creditAmount) $scope.rows.push(({amount:a,date:rightnow}); //if($scope.creditAmount) $scope.rows.push(+$scope.creditAmount);
     if($scope.creditAmount) $scope.rows.push(+$scope.creditAmount);
     else $scope.temp = false;
   };
+
+  $scope.todaysDate = function(){
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    } 
+
+    if(mm<10) {
+        mm='0'+mm
+    } 
+
+    today = dd+'/'+mm+'/'+yyyy;
+
+    return today;
+
+  }
 
   $scope.debitTemp = function(){
     if($scope.temp) $scope.rows.pop(); 
